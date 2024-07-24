f47e90278e47552d50a1d52c9a73248168eabc5d
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-07-25 00:34:28 +0200

    Added configuration to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use zero2prod::{configuration::get_configuration, run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // Initialise our configuration reader
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
     // Returns a io::Error if we fail to bind the address
     // otherwise call .await on our server
-    let listener =
-        TcpListener::bind("127.0.0.1:8000").expect("Expected to bind port for TcpListener");
+    let listener = TcpListener::bind(address).expect("Expected to bind port for TcpListener");
     run(listener)?.await
 }
