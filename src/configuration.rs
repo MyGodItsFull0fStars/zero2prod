192a532dc0797106db87c647e9b537f938cd9641
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-07-27 17:26:31 +0200

    Added comments and restructured code

--- src/configuration.rs
+++ src/configuration.rs
@@ -13,20 +13,6 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
-pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    // Initialise our configuration reader
-    let settings = config::Config::builder()
-        // Add configuration values from a file named `configuration.yaml`
-        .add_source(config::File::new(
-            "configuration.yaml",
-            config::FileFormat::Yaml,
-        ))
-        .build()?;
-    // Try to convert the configuration values it read into
-    // our Settings type
-    settings.try_deserialize::<Settings>()
-}
-
 impl DatabaseSettings {
     pub fn connection_string(&self) -> String {
         format!(
@@ -34,4 +20,21 @@ impl DatabaseSettings {
             self.username, self.password, self.host, self.port, self.database_name
         )
     }
+
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
+    }
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()?;
+    settings.try_deserialize::<Settings>()
 }
