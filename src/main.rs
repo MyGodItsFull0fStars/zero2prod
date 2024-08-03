879fe72946a9c9e62e9b4ba6008a66de5df291a9
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-08-03 02:22:10 +0200

    Added telemetry.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use std::net::TcpListener;
 
-use env_logger::Env;
-use sqlx::PgPool;
-use zero2prod::configuration::get_configuration;
+use sqlx::{PgPool, Pool, Postgres};
+
+use zero2prod::configuration::{get_configuration, Settings};
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     // Initialise our configuration reader
-    let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres");
+    let configuration: Settings = get_configuration().expect("Failed to read configuration");
+    let connection_pool: Pool<Postgres> =
+        PgPool::connect(&configuration.database.connection_string())
+            .await
+            .expect("Failed to connect to Postgres");
 
     // Here we choose to bind explicitly to localhost, 127.0.0.1, for security
     // reasons. This binding may cause issues in some environments. For example,
@@ -20,10 +24,11 @@ async fn main() -> std::io::Result<()> {
     // server when it is bound to WSL2's localhost interface. As a workaround,
     // you can choose to bind to all interfaces, 0.0.0.0, instead, but be aware
     // of the security implications when you expose the server on all interfaces.
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address: String = format!("127.0.0.1:{}", configuration.application_port);
     // Returns a io::Error if we fail to bind the address
     // otherwise call .await on our server
-    let listener = TcpListener::bind(address).expect("Expected to bind port for TcpListener");
+    let listener: TcpListener =
+        TcpListener::bind(address).expect("Expected to bind port for TcpListener");
     run(listener, connection_pool)?.await?;
 
     Ok(())
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -23,31 +23,32 @@ impl FormData {
     }
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name,
+    )
+)]
 pub async fn subscribe(
     form: web::Form<FormData>,
     // Retrieving a connection from the application state!
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    // generate a random unique identifier
-    let request_id = Uuid::new_v4();
-    // Spans like logs have an associated level
-    // `info_span` creates a span at the info level
-    let request_span = tracing::info_span!(
-        "Adding new subscriber.",
-        // this notation is called `structured information`
-        // the % before a value tells `tracing` to use the `Display` implementation of the value
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name = %form.name,
-    );
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
 
-    // Using `enter` in an async function is a recipe for disaster!
-    // So don't try this at home!!
-    let _request_span_guard = request_span.enter();
-
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-
-    match sqlx::query!(
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -59,15 +60,12 @@ pub async fn subscribe(
     )
     // Using the pool as a drop-in replacement for a single DBConnection
     // Allows us to pool the queries concurrently
-    .execute(pool.get_ref())
+    .execute(pool)
     // First we attach the instrumentation, then we `await` it
-    .instrument(query_span)
     .await
-    {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            tracing::error!("Failed to execute query: {:?}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
