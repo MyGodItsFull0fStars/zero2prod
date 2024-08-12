8ec3693e083ada402a4796b81d65a055fc873064
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-08-12 16:35:25 +0200

    removed request_id field from FormData, as it is included via tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -26,7 +26,6 @@ impl FormData {
     name = "Adding a new subscriber",
     skip(form, pool),
     fields(
-        request_id = %Uuid::new_v4(),
         subscriber_email = %form.email,
         subscriber_name = %form.name,
     )
